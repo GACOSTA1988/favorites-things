e12838a5c3f173b098626b969b735a39f8a4d12c
Student <example@example.com>
2019-10-28 14:09:44 -0700

    add last 3 functions on favs

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,24 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
-    console.log("test");
+    // console.log("test");
 
     var food = $("#food").val();
     var color = $("#color").val();
     var animal = $("#animal").val();
 
     var favorites = [food , color , animal ];
+    var subFavs = favorites[1] + favorites[2];
+    var thirdFavs = [];
+        thirdFavs.push(subFavs);
 
     $("#favoritesLists").text(favorites);
+    $("#favoritesLists2").text(subFavs);
+    $("#favoritesLists3").text(thirdFavs);
+    // $("#favoritesLists").text(favorites[1]);
+    // $("#favoritesLists").text(favorites[0]);
+
+    // $("#favoritesLists").text(favorites);
     // alert(favorites);
     // $("#results").show();
   });
